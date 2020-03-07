9174390c5a57003941044e9d732c32a42f75c1e1
Axe Tang <axetang@163.com>
2020-03-07 23:44:13 +0800

    loc

--- MatPlotLib/Beginner/ch01/1.py
+++ MatPlotLib/Beginner/ch01/1.py
@@ -10,3 +10,17 @@ plt.plot(x, y, ls="-", lw=5, label="Plot figure by Axe Tang")
 plt.scatter(x, z, c='b', label='scatterd figure')
 plt.legend(loc="upper center")
 plt.show()
+'''
+legend的loc参数：文本标签位置
+        best
+        upper right
+        upper left
+        lower left
+        lower right
+        right
+        center left
+        center right
+        lower center
+        upper center
+        center
+'''
