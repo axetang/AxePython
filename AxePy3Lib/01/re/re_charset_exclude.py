1d43062a77f95be188afef6208870dd3125995db
Axe Tang <axetang@163.com>
2020-03-11 21:49:58 +0800

    re

--- AxePy3Lib/01/re/re_charset_exclude.py
+++ AxePy3Lib/01/re/re_charset_exclude.py
@@ -6,10 +6,18 @@
 """Repetition of patterns
 """
 
-#end_pymotw_header
+# end_pymotw_header
 from re_test_patterns import test_patterns
 
 test_patterns(
     'This is some text -- with punctuation.',
     [('[^-. ]+', 'sequences without -, ., or space')],
+)
+test_patterns(
+    'This + is /some  text --() with punctuation.',
+    [('[^-./+() ]+', 'sequences without -, ., or space')],
+)
+test_patterns(
+    'This + is /some  text --() with punctuation.',
+    [('[-./+() ]+', 'sequences without -, ., or space')],
 )
--- AxePy3Lib/01/re/re_test_patterns.py
+++ AxePy3Lib/01/re/re_test_patterns.py
@@ -23,8 +23,8 @@ def test_patterns(text, patterns):
             s = match.start()
             e = match.end()
             substr = text[s:e]
-            #n_backslashes = text[:s].count('\\')
-            #prefix = '.' * (s + n_backslashes)
+            # n_backslashes = text[:s].count('\\')
+            # prefix = '.' * (s + n_backslashes)
             prefix = '.'*s
             print("  {}'{}'".format(prefix, substr))
         print()
@@ -34,3 +34,16 @@ def test_patterns(text, patterns):
 if __name__ == '__main__':
     test_patterns('abbaaabbbbaaaaa', [('ab', "'a' followed by 'b'"),
                                       ('ba', "'b' followed by 'a'")])
+    print("-----------------------")
+    text = 'abbaaabbbbaaaaa'
+    # patts = [['a', 'b'], ['c', 'd'], ['e', 'f']]
+    # patts = [('a', 'b'), ('c', 'd'), ('e', 'f')]
+    # for pat, desc in patts:
+    patts = {'ab': '12', 'ba': '21', 'aba': '121'}
+    for pat, desc in patts.items():
+        print("pat is {0!r}, desc is {1!r}".format(pat, desc))
+        for match in re.finditer(pat, text):
+            s = match.start()
+            e = match.end()
+            substr = text[s:e]
+            print(s, e, substr)
